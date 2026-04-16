4b0a976eae14944e5fca180222e60a4d33c6ad4e
LuizLamim <luiz.britohenrique@hotmail.com>
2026-04-16 19:30:52 -0300

    3

--- calculadora.py
+++ calculadora.py
@@ -6,3 +6,11 @@ def calculadora():
     print("4. Dividir")
 
     escolha = input("\nEscolha a operação (1/2/3/4): ")
+
+    if escolha in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+        except ValueError:
+            print("Por favor, digite apenas números válidos.")
+            return
