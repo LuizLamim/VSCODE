668742a5ddee4b63a652135fb647cb201a889ece
LuizLamim <luiz.britohenrique@hotmail.com>
2026-04-17 20:32:53 -0300

    2

--- Lorenz.py
+++ Lorenz.py
@@ -1,2 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def lorenz_derivadas(x, y, z, sigma=10.0, rho=28.0, beta=8.0/3.0):
+    """
+    Calcula as derivadas parciais do sistema de Lorenz.
+    """
+    dx_dt = sigma * (y - x)
+    dy_dt = x * (rho - z) - y
+    dz_dt = x * y - beta * z
+    return dx_dt, dy_dt, dz_dt
