5f6dc125af532cadc389b27f1eb4529a3e2670ee
LuizLamim <luiz.britohenrique@hotmail.com>
2026-04-17 20:33:48 -0300

    3

--- Lorenz.py
+++ Lorenz.py
@@ -9,3 +9,7 @@ def lorenz_derivadas(x, y, z, sigma=10.0, rho=28.0, beta=8.0/3.0):
     dy_dt = x * (rho - z) - y
     dz_dt = x * y - beta * z
     return dx_dt, dy_dt, dz_dt
+
+# Parâmetros da simulação
+dt = 0.01          # Tamanho do passo de tempo
+num_passos = 10000 # Número total de iterações 
