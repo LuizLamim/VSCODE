91bc0162f321e8fca91b703de35e6138f4b9d5a8
LuizLamim <luiz.britohenrique@hotmail.com>
2026-04-23 21:17:13 -0300

    input

--- trianguloretangulo.py
+++ trianguloretangulo.py
@@ -1,1 +1,9 @@
 import math
+
+def calcular_triangulo():
+    print("--- Calculadora de Triângulo Retângulo ---")
+    print("O que você deseja fornecer?")
+    print("1. Os dois catetos")
+    print("2. A hipotenusa e um cateto")
+    
+    opcao = input("Escolha a opção (1 ou 2): ")
