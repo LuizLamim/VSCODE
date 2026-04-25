52ae843c52047d562256db6cfa46ecd14854844a
LuizLamim <luiz.britohenrique@hotmail.com>
2026-04-25 19:40:08 -0300

    Intervalo

--- plot_exponencial.py
+++ plot_exponencial.py
@@ -1,2 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Define o intervalo de valores para x (ex: de -5 até 5, com 100 pontos)
+x = np.linspace(-5, 5, 100)
+
+# Calcula a função exponencial e^x para cada valor de x
+y = np.exp(x)
