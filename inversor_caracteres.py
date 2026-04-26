ca103a6ed20d19492a266442f81477032dc853b9
LuizLamim <luiz.britohenrique@hotmail.com>
2026-04-26 12:59:13 -0300

    Inverter o texto

--- inversor_caracteres.py
+++ inversor_caracteres.py
@@ -1,2 +1,5 @@
 # Solicita ao usuário que digite uma string
 texto_original = input("Digite um texto para inverter: ")
+
+# Inverte a string usando a técnica de fatiamento (slicing)
+texto_invertido = texto_original[::-1]
